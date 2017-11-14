80bda639fddaa13e5f0bc1e99d871198252119cc
owen <thss15_zhangqr@163.com>
2017-11-14 21:31:51 +0800

    debug in git

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -4,9 +4,8 @@ from .models import Post, Category, Tag
 
 
 class PostAdmin(admin.ModelAdmin):
-	list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
-
