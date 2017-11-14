36f0b0f2148e720e81f93348caa7980497bfa76d
owen <thss15_zhangqr@163.com>
2017-11-14 18:58:38 +0800

    add index view

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 def index(request):
-    return render(request, 'blog/index.html', context={
-            'title': '首页',
-            'welcome': 'welcome'
-        }')
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'mainsite/index.html', context={'post_list': post_list})
