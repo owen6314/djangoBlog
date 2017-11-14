1f499416bb98a5f3e5ce0eef6db86b2f236a86ad
owen <thss15_zhangqr@163.com>
2017-11-14 20:20:20 +0800

    finish detail view

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category, Tag
 # Register your models here.
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'pub_date')
+	list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
+
--- mainsite/models.py
+++ mainsite/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -31,4 +32,7 @@ class Post(models.Model):
     author = models.ForeignKey(User)
 
     def __str__(self):
-        return self.name
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('mainsite:detail', kwargs={'pk': self.pk})
