a3f263b054d73a12511e306a7cab7ac24a15e78d
owen <thss15_zhangqr@163.com>
2017-11-14 21:18:09 +0800

    adjust code style

--- mainsite/views.py
+++ mainsite/views.py
@@ -7,12 +7,11 @@ def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'mainsite/index.html', context={'post_list': post_list})
 
-def detail(request, pk):
-	post = get_object_or_404(Post, pk=pk)
-	post.body = markdown.markdown(post.body, extensions = [
-												'markdown.extensions.extra',
-												'markdown.extensions.codehilite',
-												'markdown.extensions.toc'
 
-		                                      ])	
-	return render(request, 'mainsite/detail.html', context={'post': post})
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body, extensions=[
+                                  'markdown.extensions.extra',
+                                  'markdown.extensions.codehilite',
+                                  'markdown.extensions.toc'])
+    return render(request, 'mainsite/detail.html', context={'post': post})
