6978e758e0290e44679cfb7a5cf4b81ef6562687
owen <thss15_zhangqr@163.com>
2017-11-16 08:54:31 +0800

    change configs setting

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -11,21 +11,26 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Configs
+CONFIGS = json.loads(open(os.path.join(BASE_DIR, 'configs.json')).read())
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z3e41drring3(2w8lj)ga^k@7t-!#i6+dx@+e-am)-#__-)4cp'
+SECRET_KEY = CONFIGS['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = CONFIGS['DEBUG']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = CONFIGS['ALLOWED_HOSTS']
 
 
 # Application definition
@@ -120,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
